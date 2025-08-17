23270eb26c8241a0a0cc3e3b576bbced4cd9f020
AmandaAnte <np3kqp977aq4@hotmail.com>
2025-08-17 16:43:52 +0800

    Update FootballBetting.sol

--- contracts/FootballBetting.sol
+++ contracts/FootballBetting.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {FHE, euint32, externalEuint32, ebool} from "@fhevm/solidity/lib/FHE.sol";
+import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 contract FootballBetting is SepoliaConfig {
     uint256 public constant FOOT_POINT_RATIO = 100000; // 1 ETH = 100000 FootPoint
-    uint256 public constant BET_AMOUNT = 10; // 每注10积分
 
     enum BetType { HOME_WIN, AWAY_WIN, DRAW }
     enum GameStatus { BETTING_OPEN, BETTING_CLOSED, SETTLED }
@@ -17,17 +16,14 @@ contract FootballBetting is SepoliaConfig {
         uint256 startTime;
         GameStatus status;
         BetType result;
-        euint32 totalHomeBets;
-        euint32 totalAwayBets;
-        euint32 totalDrawBets;
-        mapping(address => euint32) userHomeBets;
-        mapping(address => euint32) userAwayBets;
-        mapping(address => euint32) userDrawBets;
     }
 
     address public owner;
     mapping(address => euint32) private userFootPoints;
     mapping(uint256 => Game) public games;
+    mapping(uint256 => mapping(address => euint32)) public userHomeBets;
+    mapping(uint256 => mapping(address => euint32)) public userAwayBets;
+    mapping(uint256 => mapping(address => euint32)) public userDrawBets;
     uint256 public gameCounter;
 
     event GameCreated(uint256 indexed gameId, string homeTeam, string awayTeam, uint256 startTime);
@@ -59,9 +55,8 @@ contract FootballBetting is SepoliaConfig {
         require(msg.value > 0, "Must send ETH");
         
         uint256 footPointsToAdd = msg.value * FOOT_POINT_RATIO / 1 ether;
-        euint32 encryptedPoints = FHE.asEuint32(footPointsToAdd);
         
-        userFootPoints[msg.sender] = FHE.add(userFootPoints[msg.sender], encryptedPoints);
+        userFootPoints[msg.sender] = FHE.add(userFootPoints[msg.sender], uint32(footPointsToAdd));
         
         FHE.allowThis(userFootPoints[msg.sender]);
         FHE.allow(userFootPoints[msg.sender], msg.sender);
@@ -84,9 +79,6 @@ contract FootballBetting is SepoliaConfig {
         game.awayTeam = awayTeam;
         game.startTime = startTime;
         game.status = GameStatus.BETTING_OPEN;
-        game.totalHomeBets = FHE.asEuint32(0);
-        game.totalAwayBets = FHE.asEuint32(0);
-        game.totalDrawBets = FHE.asEuint32(0);
         
         emit GameCreated(gameId, homeTeam, awayTeam, startTime);
     }
@@ -98,43 +90,32 @@ contract FootballBetting is SepoliaConfig {
         bytes calldata inputProof
     ) external gameExists(gameId) bettingOpen(gameId) {
         euint32 betCount = FHE.fromExternal(encryptedBetCount, inputProof);
-        euint32 totalBetAmount = FHE.mul(betCount, BET_AMOUNT);
         
-        ebool hasSufficientPoints = FHE.le(totalBetAmount, userFootPoints[msg.sender]);
-        
-        euint32 actualBetAmount = FHE.select(hasSufficientPoints, totalBetAmount, FHE.asEuint32(0));
-        euint32 actualBetCount = FHE.select(hasSufficientPoints, betCount, FHE.asEuint32(0));
-        
-        userFootPoints[msg.sender] = FHE.sub(userFootPoints[msg.sender], actualBetAmount);
-        
-        Game storage game = games[gameId];
-        
+        // Add the bet to user's total for this bet type
         if (betType == BetType.HOME_WIN) {
-            game.userHomeBets[msg.sender] = FHE.add(game.userHomeBets[msg.sender], actualBetCount);
-            game.totalHomeBets = FHE.add(game.totalHomeBets, actualBetCount);
-            FHE.allowThis(game.userHomeBets[msg.sender]);
-            FHE.allow(game.userHomeBets[msg.sender], msg.sender);
+            userHomeBets[gameId][msg.sender] = FHE.add(userHomeBets[gameId][msg.sender], betCount);
+            FHE.allowThis(userHomeBets[gameId][msg.sender]);
+            FHE.allow(userHomeBets[gameId][msg.sender], msg.sender);
         } else if (betType == BetType.AWAY_WIN) {
-            game.userAwayBets[msg.sender] = FHE.add(game.userAwayBets[msg.sender], actualBetCount);
-            game.totalAwayBets = FHE.add(game.totalAwayBets, actualBetCount);
-            FHE.allowThis(game.userAwayBets[msg.sender]);
-            FHE.allow(game.userAwayBets[msg.sender], msg.sender);
+            userAwayBets[gameId][msg.sender] = FHE.add(userAwayBets[gameId][msg.sender], betCount);
+            FHE.allowThis(userAwayBets[gameId][msg.sender]);
+            FHE.allow(userAwayBets[gameId][msg.sender], msg.sender);
         } else {
-            game.userDrawBets[msg.sender] = FHE.add(game.userDrawBets[msg.sender], actualBetCount);
-            game.totalDrawBets = FHE.add(game.totalDrawBets, actualBetCount);
-            FHE.allowThis(game.userDrawBets[msg.sender]);
-            FHE.allow(game.userDrawBets[msg.sender], msg.sender);
+            userDrawBets[gameId][msg.sender] = FHE.add(userDrawBets[gameId][msg.sender], betCount);
+            FHE.allowThis(userDrawBets[gameId][msg.sender]);
+            FHE.allow(userDrawBets[gameId][msg.sender], msg.sender);
         }
-        
-        FHE.allowThis(userFootPoints[msg.sender]);
-        FHE.allow(userFootPoints[msg.sender], msg.sender);
         
         emit BetPlaced(gameId, msg.sender, betType);
     }
 
-    function getUserBets(uint256 gameId, address user) external view gameExists(gameId) returns (euint32, euint32, euint32) {
-        Game storage game = games[gameId];
-        return (game.userHomeBets[user], game.userAwayBets[user], game.userDrawBets[user]);
+    function getUserBets(uint256 gameId, address user) 
+        external 
+        view 
+        gameExists(gameId) 
+        returns (euint32, euint32, euint32) 
+    {
+        return (userHomeBets[gameId][user], userAwayBets[gameId][user], userDrawBets[gameId][user]);
     }
 
     function closeBetting(uint256 gameId) external onlyOwner gameExists(gameId) {
@@ -153,60 +134,6 @@ contract FootballBetting is SepoliaConfig {
         emit GameSettled(gameId, result);
     }
 
-    function claimWinnings(uint256 gameId) external gameExists(gameId) {
-        require(games[gameId].status == GameStatus.SETTLED, "Game not settled");
-        
-        Game storage game = games[gameId];
-        BetType result = game.result;
-        
-        euint32 userWinningBets;
-        euint32 totalWinningBets;
-        euint32 totalLosingBets;
-        
-        if (result == BetType.HOME_WIN) {
-            userWinningBets = game.userHomeBets[msg.sender];
-            totalWinningBets = game.totalHomeBets;
-            totalLosingBets = FHE.add(game.totalAwayBets, game.totalDrawBets);
-        } else if (result == BetType.AWAY_WIN) {
-            userWinningBets = game.userAwayBets[msg.sender];
-            totalWinningBets = game.totalAwayBets;
-            totalLosingBets = FHE.add(game.totalHomeBets, game.totalDrawBets);
-        } else {
-            userWinningBets = game.userDrawBets[msg.sender];
-            totalWinningBets = game.totalDrawBets;
-            totalLosingBets = FHE.add(game.totalHomeBets, game.totalAwayBets);
-        }
-        
-        ebool hasWinningBets = FHE.gt(userWinningBets, FHE.asEuint32(0));
-        ebool hasWinners = FHE.gt(totalWinningBets, FHE.asEuint32(0));
-        
-        euint32 originalBetAmount = FHE.mul(userWinningBets, BET_AMOUNT);
-        
-        euint32 shareOfLosingBets = FHE.select(
-            hasWinners,
-            FHE.div(FHE.mul(totalLosingBets, userWinningBets), totalWinningBets),
-            FHE.asEuint32(0)
-        );
-        
-        euint32 rewardAmount = FHE.mul(shareOfLosingBets, BET_AMOUNT);
-        euint32 totalReward = FHE.add(originalBetAmount, rewardAmount);
-        
-        euint32 finalReward = FHE.select(hasWinningBets, totalReward, FHE.asEuint32(0));
-        
-        userFootPoints[msg.sender] = FHE.add(userFootPoints[msg.sender], finalReward);
-        
-        if (result == BetType.HOME_WIN) {
-            game.userHomeBets[msg.sender] = FHE.asEuint32(0);
-        } else if (result == BetType.AWAY_WIN) {
-            game.userAwayBets[msg.sender] = FHE.asEuint32(0);
-        } else {
-            game.userDrawBets[msg.sender] = FHE.asEuint32(0);
-        }
-        
-        FHE.allowThis(userFootPoints[msg.sender]);
-        FHE.allow(userFootPoints[msg.sender], msg.sender);
-    }
-
     function getGameInfo(uint256 gameId) external view gameExists(gameId) returns (
         string memory homeTeam,
         string memory awayTeam,
@@ -215,11 +142,6 @@ contract FootballBetting is SepoliaConfig {
     ) {
         Game storage game = games[gameId];
         return (game.homeTeam, game.awayTeam, game.startTime, game.status);
-    }
-
-    function getGameTotalBets(uint256 gameId) external view gameExists(gameId) returns (euint32, euint32, euint32) {
-        Game storage game = games[gameId];
-        return (game.totalHomeBets, game.totalAwayBets, game.totalDrawBets);
     }
 
     function withdrawETH() external onlyOwner {
